5bbf8a38db8eaf27aa05441a57e2923601816c0f
Marcin Radoszewski <moriturius@gmail.com>
2023-10-31 08:17:04 +0100

    Added installation of tools

--- install.ps1
+++ install.ps1
@@ -1,3 +1,62 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Path
 
-New-Item -ItemType SymbolicLink -Target "$scriptPath/nvim" -Path "$HOME/AppData/Local/nvim"
+# Setup neovim config files
+if (!$(Test-Path -Path "$HOME/AppData/Local/nvim")) {
+    New-Item -ItemType SymbolicLink -Target "$scriptPath/nvim" -Path "$HOME/AppData/Local/nvim"
+} else {
+    Write-Output "Neovim folder already in place."
+}
+
+# Setup wezterm config
+if (!$(Test-Path -Path "$HOME/.wezterm.lua")) {
+    New-Item -ItemType SymbolicLink -Target "$scriptPath/wezterm.lua" -Path "$HOME/.wezterm.lua"
+} else {
+    Write-Output "Wezterm configuration already in place."
+}
+
+# Setup PowerShell profile
+if (Test-Path -Path $profile) {
+    Remove-Item -Path $profile
+}
+New-Item -ItemType SymbolicLink -Target "$scriptPath/profile.ps1" -Path "$profile" | Out-Null
+
+function Install-Winget-Package {
+    param (
+        $Package,
+        $Command
+    )
+
+    if (!$(get-command $Command -ErrorAction SilentlyContinue)) {
+        winget install $Package
+    } else {
+        Write-Output "$Command already installed."
+    }
+}
+
+function Install-Cargo-Package {
+    param (
+        $Package,
+        $Command
+    )
+
+    if (!$(get-command $Command -ErrorAction SilentlyContinue)) {
+        cargo install $Package
+    } else {
+        Write-Output "$Command already installed."
+    }
+}
+
+Install-Winget-Package -Package neovim -Command nvim
+Install-Winget-Package -Package wezterm -Command wezterm
+
+$cargoInstalled = get-command cargo -ErrorAction SilentlyContinue
+if ($cargoInstalled) {
+    Install-Cargo-Package -Package eza -Command eza
+    Install-Cargo-Package -Package fzf -Command fzf
+    Install-Cargo-Package -Package ripgrep -Command rg
+    Install-Cargo-Package -Package bat -Command bat
+    Install-Cargo-Package -Package starship -Command starship
+} else {
+    Write-Output "Install rust's cargo so I can install other required tools!"
+}
+
---
+++ profile.ps1
@@ -0,0 +1,5 @@
+Set-Alias ls eza
+Set-Alias cat bat
+New-Alias grep rg
+
+Invoke-Expression (&starship init powershell)
